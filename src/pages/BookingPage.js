b8cc2dc758a40721e2ab285245932378d0383bcf
Oscar Jimenez <oscar.jimenez@wundermanthompson.com>
2024-10-07 15:05:08 +0200

    Added new components, HomePage and BookingPage

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,9 @@
+function BookingPage() {
+	return (
+		<main>
+			<h1>Booking Page</h1>
+		</main>
+	);
+}
+
+export default BookingPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+function HomePage() {
+    return (
+        <>
+            <Header />
+			<main>
+				<h1>Home Page</h1>
+			</main>
+            <Footer />
+        </>
+    );
+}
+
+export default HomePage;
